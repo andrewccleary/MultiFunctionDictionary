2b3ef47771bea4bb90fad62562b2786a02ff9040
Andrew Cleary <acclea01@louisville.edu>
2019-02-07 17:01:08 -0500

    Initial Commit

---
+++ MultifunctionalDictionary/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultifunctionalDictionary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bookSelector.IsEditable = true;
+            bookSelector.IsReadOnly = true;
+            bookSelector.Text = "Book";
+
+            chapterSelector.IsEditable = true;
+            chapterSelector.IsReadOnly = true;
+            chapterSelector.Text = "Chapter";
+
+            verseSelector.IsEditable = true;
+            verseSelector.IsReadOnly = true;
+            verseSelector.Text = "Verse";
+        }
+
+        private void BookSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
