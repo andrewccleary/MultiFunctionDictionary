a84e0f680528b60465a49d4fe2958e4a707c63fa
Andrew Cleary <acclea01@louisville.edu>
2019-03-25 21:15:47 -0400

    Update 5_populateWordMap.sql

--- PostgreSQL/Populate Scripts/5_populateWordMap.sql
+++ PostgreSQL/Populate Scripts/5_populateWordMap.sql
@@ -8,7 +8,7 @@ CREATE TABLE RAWINDEX (
 	versepos INT,
 	wordid INT,
 	word varchar(50),
-	punc varchar(2),
+	punc varchar(3),
 	italic INT,
 	cparen INT,
 	oparen INT,
@@ -20,15 +20,16 @@ CREATE TABLE RAWINDEX (
 
 COPY RAWINDEX FROM 'C:\MultifunctionalDictionary\rawindex.csv' WITH CSV ESCAPE as '"';
 
-ALTER TABLE rawindex DROP COLUMN bookid, DROP COLUMN chapter, DROP COLUMN paraid, DROP COLUMN sentid, DROP COLUMN punc, DROP COLUMN italic, DROP COLUMN cparen,DROP COLUMN oparen, DROP COLUMN placeid, DROP COLUMN syllables, DROP COLUMN personid, DROP COLUMN runcharlength DROP COLUMN yearnum;
+ALTER TABLE rawindex DROP COLUMN bookid, DROP COLUMN chapter, DROP COLUMN paraid, DROP COLUMN sentid, DROP COLUMN punc, DROP COLUMN italic, DROP COLUMN cparen, DROP COLUMN oparen, DROP COLUMN placeid, DROP COLUMN syllables, DROP COLUMN personid, DROP COLUMN yearnum;
 
 SELECT word, verseid, strongsid INTO tempwordmap
 FROM rawindex
 INNER JOIN rawwordmap
-ON rawindex.wordid = rawwordmap.wordid
+ON rawindex.wordid = rawwordmap.wordid;
 
 INSERT INTO wordmap(word, verseid, referencenum)
 SELECT word, verseid, strongsid FROM tempwordmap
 ON CONFLICT DO NOTHING;
 
 DROP TABLE tempwordmap;
+
